6e6b65db92c20de7aa3ec963c4edc2df02668d6d
Kristof Achten <kristofachten@gmail.com>
2019-12-07 23:40:11 +0100

    Day 7 input file added, d8 skel

---
+++ main/d8Skel.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func day8() {
+	start := time.Now()
+
+	fmt.Println("Day 8: solution one is " + "NOT IMPLEMENTED")
+	fmt.Println("Day 8: solution two is " + "NOT IMPLEMENTED")
+
+	fmt.Printf("DAY 8 STATS: Execution took %s\n\n", time.Since(start))
+}
--- main/main.go
+++ main/main.go
@@ -15,6 +15,7 @@ func main() {
 	day5() // https://adventofcode.com/2019/day/5
 	day6() // https://adventofcode.com/2019/day/6
 	day7() // https://adventofcode.com/2019/day/7
+	day8() // https://adventofcode.com/2019/day/8
 
 	fmt.Println("**********************************************")
 	fmt.Printf("ELAPSED TIME OVER ALL CHALLENGES: %s \n", time.Since(start))
