e6e20738caadd7a7d81af71db332407b7d7dcf33
Kristof Achten <kristofachten@gmail.com>
2019-12-10 17:40:55 +0100

    wip

--- main/main.go
+++ main/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	start := time.Now()
 
-	day1()  // https://adventofcode.com/2019/day/1
+	/*day1()  // https://adventofcode.com/2019/day/1
 	day2()  // https://adventofcode.com/2019/day/2
 	day3()  // https://adventofcode.com/2019/day/3
 	day4()  // https://adventofcode.com/2019/day/4
@@ -16,7 +16,7 @@ func main() {
 	day6()  // https://adventofcode.com/2019/day/6
 	day7()  // https://adventofcode.com/2019/day/7
 	day8()  // https://adventofcode.com/2019/day/8
-	day9()  // https://adventofcode.com/2019/day/9
+	day9()  // https://adventofcode.com/2019/day/9*/
 	day10() // https://adventofcode.com/2019/day/10
 
 	fmt.Println("**********************************************")
