9499213818f0c33ddc1ef3671ed3a97c7ae0e23f
Kristof Achten <kristofachten@gmail.com>
2019-12-13 23:56:00 +0100

    temp

--- main/d13Skel.go
+++ main/d13Skel.go
@@ -10,8 +10,8 @@ const (
 	empty  = " "
 	wall   = "#"
 	block  = "B"
-	paddle = "P"
-	ball   = "@"
+	paddle = "="
+	ball   = "O"
 )
 
 func day13() {
@@ -19,9 +19,7 @@ func day13() {
 
 	input := stringSliceToIntSlice(strings.Split(string(getPuzzleInput("input/day13.txt")), ","))
 
-	fmt.Println(countBlocks(runGame(input)))
-	printWorld(runGame(input))
-	fmt.Println("Day 13: solution one is " + "NOT IMPLEMENTED")
+	fmt.Println("Day 13: solution one is ", countBlocks(runGame(input)))
 	fmt.Println("Day 13: solution two is " + "NOT IMPLEMENTED")
 
 	fmt.Printf("DAY 13 STATS: Execution took %s\n\n", time.Since(start))
@@ -31,7 +29,7 @@ func runGame(code []int64) [][]string {
 	world := make([][]string, 24)
 
 	for i := 0; i < 24; i++ {
-		world[i] = make([]string, 50)
+		world[i] = make([]string, 45)
 	}
 	state := createDefaultIntcodeState(code, []int64{})
 
@@ -46,37 +44,28 @@ func runGame(code []int64) [][]string {
 		v, _, thrdNewState := runIntCode(sndNewState)
 
 		state = thrdNewState
-
-		var add string
-		switch v {
-		case 0:
-			add = empty
-		case 1:
-			add = wall
-		case 2:
-			add = block
-		case 3:
-			add = paddle
-		case 4:
-			add = ball
-		default:
-			panic("Invalid object being spawned in the world")
-		}
-
-		curValue := world[y][x]
-
-		if curValue == wall || curValue == paddle {
-			continue
-		}
-
-		if curValue == block && add != ball {
-			continue
-		}
-
-		world[y][x] = add
+		world[y][x] = i2obj(v)
 	}
 
+	printWorld(world)
 	return world
+}
+
+func i2obj(i int64) string {
+	switch i {
+	case 0:
+		return empty
+	case 1:
+		return wall
+	case 2:
+		return block
+	case 3:
+		return paddle
+	case 4:
+		return ball
+	default:
+		panic("Invalid object")
+	}
 }
 
 func countBlocks(input [][]string) int {
