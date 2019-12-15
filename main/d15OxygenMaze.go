662948d8f3fc5ed178a525c309e4ab6f14a73be6
Kristof Achten <kristofachten@gmail.com>
2019-12-15 21:51:33 +0100

    maze drawing. Added output file generation

--- main/d15OxygenMaze.go
+++ main/d15OxygenMaze.go
@@ -6,32 +6,47 @@ import (
 	"time"
 )
 
-type intcodeNode struct {
-	val      int64
-	children []intcodeNode
+type cell struct {
+	val        int64
+	neighbours []cell
+	physPos    coords
 }
 
 var visited map[coords]bool
+var maze [][]string
 
 func day15() {
 	start := time.Now()
 
 	visited = make(map[coords]bool)
+	maze = make([][]string, 41)
+	for i := 0; i < 41; i++ {
+		maze[i] = make([]string, 41)
+		for j := 0; j < 41; j++ {
+			maze[i][j] = "#"
+		}
+	}
 
 	input := strings.Split(string(getPuzzleInput("input/day15.txt")), ",")
-	fullMaze := buildTree(createDefaultIntcodeState(stringSliceToIntSlice(input), []int64{2}), coords{0, 0}, 0)
+	fullMaze := buildTree(createDefaultIntcodeState(stringSliceToIntSlice(input), []int64{2}), coords{21, 22}, 0)
 
 	printPuzzleResult(15, searchOxygen(fullMaze, 0), notImplemented)
+
+	fmt.Println("The maze looked like this (@ = oxygen system, # = wall):")
+	drawMaze()
 
 	fmt.Printf("DAY 15 STATS: Execution took %s\n\n", time.Since(start))
 }
 
-func buildTree(input intcodeState, loc coords, steps int) intcodeNode {
+func buildTree(input intcodeState, loc coords, steps int) cell {
 	dirs := []int64{1, 2, 3, 4}
-	var children []intcodeNode
+	var neightbours []cell
 	visited[loc] = true
 
 	output, _, newState := runIntCode(input)
+
+	maze[loc.y][loc.x] = i2viz(int(output))
+
 	steps++
 	var backtrack bool
 	if output == 0 {
@@ -43,15 +58,15 @@ func buildTree(input intcodeState, loc coords, steps int) intcodeNode {
 			newloc := updateLoc(loc, v)
 
 			if _, ok := visited[newloc]; !ok {
-				children = append(children, buildTree(intcodeState{newState.program, newState.ptr, newState.relativeBase, []int64{v}}, newloc, steps))
+				neightbours = append(neightbours, buildTree(intcodeState{newState.program, newState.ptr, newState.relativeBase, []int64{v}}, newloc, steps))
 			}
 		}
 	}
 
-	return intcodeNode{output, children}
+	return cell{output, neightbours, loc}
 }
 
-func searchOxygen(node intcodeNode, steps int) int {
+func searchOxygen(node cell, steps int) int {
 	steps++
 
 	if node.val == 2 {
@@ -59,11 +74,24 @@ func searchOxygen(node intcodeNode, steps int) int {
 	}
 
 	var stepsReq int
-	for _, v := range node.children {
+	for _, v := range node.neighbours {
 		stepsReq += searchOxygen(v, steps)
 	}
 
 	return stepsReq
+}
+
+func i2viz(input int) string {
+	switch input {
+	case 0:
+		return wall
+	case 1:
+		return empty
+	case 2:
+		return "@"
+	default:
+		panic("Wrong input")
+	}
 }
 
 func updateLoc(loc coords, v int64) coords {
@@ -78,5 +106,11 @@ func updateLoc(loc coords, v int64) coords {
 		return coords{loc.x + 1, loc.y}
 	default:
 		panic("aaaaah")
+	}
+}
+
+func drawMaze() {
+	for x := range maze {
+		fmt.Println(maze[x])
 	}
 }
--- main/main.go
+++ main/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
+	old := os.Stdout
+	wd, _ := os.Getwd()
+	f, _ := os.Create(wd + "/output.txt")
+
+	os.Stdout = f
+
 	start := time.Now()
 
-	/*day1()  // https://adventofcode.com/2019/day/1
+	day1()  // https://adventofcode.com/2019/day/1
 	day2()  // https://adventofcode.com/2019/day/2
 	day3()  // https://adventofcode.com/2019/day/3
 	day4()  // https://adventofcode.com/2019/day/4
@@ -21,10 +28,15 @@ func main() {
 	day11() // https://adventofcode.com/2019/day/11
 	day12() // https://adventofcode.com/2019/day/12
 	day13() // https://adventofcode.com/2019/day/13
-	day14() // https://adventofcode.com/2019/day/14*/
+	day14() // https://adventofcode.com/2019/day/14
 	day15() // https://adventofcode.com/2019/day/15
 
 	fmt.Println("**********************************************")
 	fmt.Printf("ELAPSED TIME OVER ALL CHALLENGES: %s \n", time.Since(start))
 	fmt.Println("**********************************************")
+
+	_ = f.Close()
+	os.Stdout = old
+
+	fmt.Println(string(getPuzzleInput("output.txt")))
 }
